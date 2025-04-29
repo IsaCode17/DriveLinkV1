cc2a2032c439184185055e1fb4795a0ef9f837fa
IsaCode17 <128940252+IsaCode17@users.noreply.github.com>
2025-04-29 10:40:49 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
     GOOGLE_CLIENT_ID = os.environ.get('392554787248-rej185egfpq6433f82tc8hq1p9bhabi1.apps.googleusercontent.com')
     GOOGLE_CLIENT_SECRET = os.environ.get('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')
     GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+    GOOGLE_REDIRECT_URI = "https://drivelinkv1.onrender.com/login/callback"
