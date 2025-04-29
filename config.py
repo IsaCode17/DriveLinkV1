25cadad563987c2a248e4a484d303586f375f2f2
IsaCode17 <128940252+IsaCode17@users.noreply.github.com>
2025-04-29 10:24:26 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+import os
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'una-clave-secreta-muy-segura'
+    GOOGLE_CLIENT_ID = os.environ.get('392554787248-rej185egfpq6433f82tc8hq1p9bhabi1.apps.googleusercontent.com')
+    GOOGLE_CLIENT_SECRET = os.environ.get('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')
+    GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
