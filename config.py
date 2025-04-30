cc8eb68a55f2a186d8b00740844df5e5b5eb802f
IsaCode17 <128940252+IsaCode17@users.noreply.github.com>
2025-04-29 20:06:53 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ class Config:
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
     GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"  # <-- Añade esta línea
+    GOOGLE_API_SCOPES = [
+           'openid',
+           'https://www.googleapis.com/auth/userinfo.email',
+           'https://www.googleapis.com/auth/userinfo.profile',
+           'https://www.googleapis.com/auth/drive.file'
+    ]
     
     # Configuración general
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
